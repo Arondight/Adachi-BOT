7a0b1c58cbedae2889d6e2f141aeed1ae1d9ec79
Qin Fandong <shell_way@foxmail.com>
2021-05-17 14:21:36 +0800

    Rename plugin info to material

--- src/plugins/material/index.js
+++ src/plugins/material/index.js
@@ -9,7 +9,7 @@ module.exports = async Message => {
     let name    = Message.sender.nickname;
     let sendID  = type === 'group' ? groupID : userID;
     let character = msg.split(/(?<=^\S+)\s/).slice(1);
-    let cacheDir  = path.join(path.resolve(__dirname, '..', '..', '..', 'data', 'image', 'info'), '/');
+    let cacheDir  = path.join(path.resolve(__dirname, '..', '..', '..', 'data', 'image', 'material'), '/');
     let weaponURL = 'https://upload-bbs.mihoyo.com/upload/2021/04/28/75276545/553459116f6aa3e12e4323393ee24b6d_5571196186126561969.png';
     let talentURL = 'https://upload-bbs.mihoyo.com/upload/2021/04/28/75276545/d0a500156192bc55b14e623806615f93_6219160940681956086.png';
     let weeklyURL = 'https://upload-bbs.mihoyo.com/upload/2021/04/28/75276545/acc156e0bbc8dbdcbc4a5c96f429c625_6103003511668528645.png';
