ad994495c6ebfed9bb5c2800a05534fb810cde04
Qin Fandong <shell_way@foxmail.com>
2021-07-07 12:07:30 +0800

    Update plugin tools

--- src/plugins/tools/feedback.js
+++ src/plugins/tools/feedback.js
@@ -6,7 +6,7 @@ module.exports = async ( id, name, msg, type, user ) => {
     if (!(await hasAuth(user, 'feedback'))) {
         await sendPrompt(id, name, '反馈', type);
     } else {
-        await bot.sendMaster(id, `${name}(${user})托我给您带个话：\n ${info}`, type);
+        await bot.sendMaster(id, `${name}(${user})托我给您带个话：\n${info}`, type);
         await bot.sendMessage(id, '我这就去给主人带个话！', type);
     }
 }
