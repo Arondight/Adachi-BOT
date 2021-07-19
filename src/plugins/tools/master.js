304f82999d3d57db6b8484c1215f57e0678bc289
Qin Fandong <shell_way@foxmail.com>
2021-07-19 09:50:12 +0800

    Update help message

--- src/plugins/tools/master.js
+++ src/plugins/tools/master.js
@@ -1,11 +1,9 @@
 const helpMessage =
-`命令列表：
-
-🔘 带话权限     <QQ> <on|off>
-🔘 十连权限     <QQ> <on|off>
-🔘 圣遗物权限   <QQ> <on|off>
-🔘 UID权限      <QQ> <on|off>
-🔘 角色权限     <QQ> <on|off>
+`🔘 带话权限 <QQ号> <on|off>
+🔘 十连权限 <QQ号> <on|off>
+🔘 圣遗物权限 <QQ号> <on|off>
+🔘 UID权限 <QQ号> <on|off>
+🔘 角色权限 <QQ号> <on|off>
 🔘 刷新卡池`;
 
 module.exports = async ( id, type ) => {
