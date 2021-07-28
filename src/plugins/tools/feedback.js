365e79788d605b1cc68f0c2bd42f6daab81aebcc
Qin Fandong <shell_way@foxmail.com>
2021-07-28 16:43:36 +0800

    Fix feedback error

--- src/plugins/tools/feedback.js
+++ src/plugins/tools/feedback.js
@@ -3,7 +3,7 @@ const { hasAuth, sendPrompt } = require('../../utils/auth');
 module.exports = async ( id, name, msg, type, user ) => {
     let info = msg.slice(4);
 
-    if (!(await hasAuth(userID, 'feedback')) || !(await hasAuth(sendID, 'feedback'))) {
+    if (!(await hasAuth(id, 'feedback')) || !(await hasAuth(id, 'feedback'))) {
         await sendPrompt(id, name, '带话', type);
     } else {
         await bot.sendMaster(id, `${name}(${user})托我给您带个话：\n${info}`, type);
