60d4dd3fc64da9087c565ce96c5785f02dcc469c
Arondight <shell_way@foxmail.com>
2021-08-06 16:51:59 +0800

    Use Express to start server

--- src/utils/server.js
+++ src/utils/server.js
@@ -1,27 +1,10 @@
-const http = require("http");
-const fs = require("fs");
 const path = require("path");
+const express = require('express');
 
 exports.newServer = (port) => {
-  http
-    .createServer((req, res) => {
-      let url = decodeURI(req.url);
-      let file = path.resolve(__dirname, "..", "..") + url;
+  const server = express();
+  const workdir = path.resolve(__dirname, "..", "..");
 
-      fs.readFile(file, (err, data) => {
-        let type = file.substr(file.lastIndexOf(".") + 1, path.length);
-        if (err) {
-          res.writeHead(404, {
-            "content-type": `text/${type};charset="utf-8"`,
-          });
-        } else {
-          res.writeHead(200, {
-            "content-type": `text/${type};charset="utf-8"`,
-          });
-          res.write(data);
-        }
-        res.end();
-      });
-    })
-    .listen(port);
+  server.use(express.static(workdir));
+  server.listen(port);
 };
