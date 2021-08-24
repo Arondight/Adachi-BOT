5ef36497fb010b30476de276d9be9686f700eeae
Arondight <shell_way@foxmail.com>
2021-08-24 09:32:06 +0800

    一点小小的工作

--- src/utils/server.js
+++ src/utils/server.js
@@ -6,5 +6,5 @@ exports.newServer = (port) => {
   const workdir = path.resolve(__dirname, "..", "..");
 
   server.use(express.static(workdir));
-  server.listen(port,'localhost');
+  server.listen(port, "localhost");
 };
