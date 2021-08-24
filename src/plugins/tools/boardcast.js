55d2bc0337287aef0f7e1f2742f0e5b155f6599d
Arondight <shell_way@foxmail.com>
2021-08-24 10:20:28 +0800

    广播功能

--- src/plugins/tools/boardcast.js
+++ src/plugins/tools/boardcast.js
@@ -1,7 +1,7 @@
 const { isMaster } = require("../../utils/auth");
 
 module.exports = async (id, msg, type) => {
-  let text = msg.split(/(?<=\d+\S+)\s/).slice(1);
+  let [text] = msg.split(/(?<=^\S+)\s/).slice(1);
   let report;
 
   if (!isMaster(id)) {
@@ -10,24 +10,28 @@ module.exports = async (id, msg, type) => {
   }
 
   if (msg.includes("群广播")) {
-    bot.gl.forEach(async (item) => {
-      await bot.sendMessage(item.group_id, "主人发送了一条群广播：\n" + text, "group");
+    bot.gl.forEach((item) => {
+      bot.sendMessage(
+        item.group_id,
+        "主人发送了一条群广播：\n" + text,
+        "group"
+      );
       report += `${item.group_name}（${item.group_id}）\n`;
     });
 
-    report += "以上群已发送广播。"
-    await bot.sendMessage(id, list, type);
+    report += "以上群已发送广播。";
+    await bot.sendMessage(id, report, type);
     return;
   }
 
   if (msg.includes("好友广播")) {
-    bot.gl.forEach(async (item) => {
-      await bot.sendMessage(item.user_id, "主人发送了一条好友广播：\n" + text, type);
+    bot.fl.forEach((item) => {
+      bot.sendMessage(item.user_id, "主人发送了一条好友广播：\n" + text, type);
       report += `${item.nickname}（${item.user_id}）\n`;
     });
 
-    report += "以上好友已发送广播。"
-    await bot.sendMessage(id, list, type);
+    report += "以上好友已发送广播。";
+    await bot.sendMessage(id, report, type);
     return;
   }
 };
