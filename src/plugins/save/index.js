c9e9b3272816749028bdad853a0897a04ebd4ed6
Qin Fandong <shell_way@foxmail.com>
2021-09-22 15:29:23 +0800

    支持按照 QQ 号查询，Close #125

--- src/plugins/save/index.js
+++ src/plugins/save/index.js
@@ -7,53 +7,43 @@ async function Plugin(Message) {
   let groupID = Message.group_id;
   let type = Message.type;
   let sendID = type === "group" ? groupID : userID;
-  let id = await getID(msg, userID);
-  let mhyID;
+  let id = await getID(msg, userID); // 米游社 ID，这里正则限定了 msg 必然有 ID
+  let mhyID = id;
 
-  if (!id) {
-    await bot.sendMessage(
-      sendID,
-      `[CQ:at,qq=${userID}] 请正确输入通行证ID。`,
-      type
-    );
-  } else {
-    mhyID = id;
+  if (msg.includes("绑定")) {
+    if (!(await isInside("map", "user", "userID", userID))) {
+      await push("map", "user", { userID, mhyID });
 
-    if (msg.includes("绑定")) {
-      if (!(await isInside("map", "user", "userID", userID))) {
-        await push("map", "user", { userID, mhyID });
-
-        if (!(await isInside("time", "user", "mhyID", mhyID))) {
-          await push("time", "user", { mhyID, time: 0 });
-        }
-
-        await bot.sendMessage(
-          sendID,
-          `[CQ:at,qq=${userID}] 通行证绑定成功，使用【米游社】来查询游戏信息并更新你的游戏角色。`,
-          type
-        );
-      } else {
-        await bot.sendMessage(
-          sendID,
-          `[CQ:at,qq=${userID}] 您已绑定通行证，请使用【改绑 ${mhyID}】。`,
-          type
-        );
+      if (!(await isInside("time", "user", "mhyID", mhyID))) {
+        await push("time", "user", { mhyID, time: 0 });
       }
-    } else if (msg.includes("改绑")) {
-      if (await isInside("map", "user", "userID", userID)) {
-        await update("map", "user", { userID }, { mhyID });
-        await bot.sendMessage(
-          sendID,
-          `[CQ:at,qq=${userID}] 通行证改绑成功，使用【米游社】来查询游戏信息并更新你的游戏角色。`,
-          type
-        );
-      } else {
-        await bot.sendMessage(
-          sendID,
-          `[CQ:at,qq=${userID}] 您还未绑定通行证，请使用【绑定 ${mhyID}】。`,
-          type
-        );
-      }
+
+      await bot.sendMessage(
+        sendID,
+        `[CQ:at,qq=${userID}] 通行证绑定成功，使用【米游社】来查询游戏信息并更新你的游戏角色。`,
+        type
+      );
+    } else {
+      await bot.sendMessage(
+        sendID,
+        `[CQ:at,qq=${userID}] 您已绑定通行证，请使用【改绑 ${mhyID}】。`,
+        type
+      );
+    }
+  } else if (msg.includes("改绑")) {
+    if (await isInside("map", "user", "userID", userID)) {
+      await update("map", "user", { userID }, { mhyID });
+      await bot.sendMessage(
+        sendID,
+        `[CQ:at,qq=${userID}] 通行证改绑成功，使用【米游社】来查询游戏信息并更新你的游戏角色。`,
+        type
+      );
+    } else {
+      await bot.sendMessage(
+        sendID,
+        `[CQ:at,qq=${userID}] 您还未绑定通行证，请使用【绑定 ${mhyID}】。`,
+        type
+      );
     }
   }
 }
