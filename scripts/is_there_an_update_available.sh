66d4b9ed8f042366a6cddfea2349113c797cf16c
Qin Fandong <shell_way@foxmail.com>
2021-10-13 12:12:12 +0800

    更新检测脚本智商 +1 ，Ref #208

--- scripts/is_there_an_update_available.sh
+++ scripts/is_there_an_update_available.sh
@@ -6,8 +6,7 @@
 
   remoteID=$(git ls-remote -qh "$remoteUrl" "$remoteBranch" 2>/dev/null \
     | awk '{print $1}')
-  localID=$(git show-ref origin/master | awk '{print $1}')
-  currentID=$(git rev-list --max-parents=0 HEAD)
+  localID=$(git rev-list --max-parents=0 HEAD)
 
   if [[ -z "$remoteID" ]]
   then
@@ -16,19 +15,5 @@
   fi
 
   word=$([[ "$remoteID" == "$localID" ]] && echo "没有" || echo "有了")
-  echo -n "看上去项目上游主线分支${word}更新……"
-
-  if [[ "$remoteID" != "$localID" ]]
-  then
-    if [[ "$localID" != "$currentID" ]]
-    then
-      echo -n "而且本地当前分支也有了提交，"
-    fi
-
-    echo "到下面的地址看看吧！"
-    echo
-    echo "$remoteUrl"
-  else
-    echo
-  fi
+  echo -n "看上去项目上游主线分支${word}更新！"
 }
