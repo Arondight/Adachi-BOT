56a393ba9b0caa0aa25430e18d9d5655c059c811
Qin Fandong <shell_way@foxmail.com>
2021-10-14 21:25:52 +0800

    Use seconds from epoch as seed for function prophecy

--- src/plugins/tools/prophecy.js
+++ src/plugins/tools/prophecy.js
@@ -1,13 +1,13 @@
 import fetch from "node-fetch";
 
 async function prophecy(id, msg, type, user, bot) {
-  let headers = {
+  const seed = Math.round(Date.now() / 1000);
+  const headers = {
     "User-Agent":
       "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:89.0) Gecko/20100101 Firefox/89.0",
   };
-
   const response = await fetch(
-    `https://api.fanlisky.cn/api/qr-fortune/get/${id}`,
+    `https://api.fanlisky.cn/api/qr-fortune/get/${seed}`,
     {
       method: "GET",
       headers: headers,
