c35d964062ea8a8af2f3cd78f780de04da8d50a5
Mark Chen <9006264+Mark9804@users.noreply.github.com>
2021-11-16 00:13:00 +0900

    前端调试工具: Convert tabs to spaces

--- devtools/dataurl/dataurl.py
+++ devtools/dataurl/dataurl.py
@@ -8,49 +8,49 @@ import platform
 import os
 
 def init(sysargs):
-	if len(sysargs) < 2:
-		print('参数不完整，无法执行')
-		quit()
-	for arg in sysargs:
-		if re.search(r'\.json$', arg) is not None:
-			json_path = arg
-			html_module = re.search(r'aby|card|character|gacha|info|overview', arg).group(0)
-		if re.search(r'^(aby|card|character|gacha|info|overview)$', arg) is not None:
-			html_module = arg
+    if len(sysargs) < 2:
+        print('参数不完整，无法执行')
+        quit()
+    for arg in sysargs:
+        if re.search(r'\.json$', arg) is not None:
+            json_path = arg
+            html_module = re.search(r'aby|card|character|gacha|info|overview', arg).group(0)
+        if re.search(r'^(aby|card|character|gacha|info|overview)$', arg) is not None:
+            html_module = arg
 
-	return json_path, html_module
+    return json_path, html_module
 
 def loadfile(jsonfile):
-	with open(jsonfile, 'r', encoding='utf-8') as f:
-		raw_data = json.load(f)
-		raw_data = json.dumps(raw_data).encode('utf-8')
-		f.close()
+    with open(jsonfile, 'r', encoding='utf-8') as f:
+        raw_data = json.load(f)
+        raw_data = json.dumps(raw_data).encode('utf-8')
+        f.close()
 
-	return raw_data
+    return raw_data
 
 def generateurl(html_page, raw_data):
-	url_prefix = 'http://localhost:9934/src/views/genshin-' + html_page + '.html?data='
-	data = base64.b64encode(raw_data).decode('utf-8')
-	url = url_prefix + data
+    url_prefix = 'http://localhost:9934/src/views/genshin-' + html_page + '.html?data='
+    data = base64.b64encode(raw_data).decode('utf-8')
+    url = url_prefix + data
 
-	return url
+    return url
 
 def openurl(url):
-	userOS = platform.system()
-	if userOS == 'Windows':
-		cmd = 'start'
-	elif userOS == 'Darwin':
-		cmd = 'open'
-	elif userOS == 'Linux':
-		cmd = 'xdg-open'
+    userOS = platform.system()
+    if userOS == 'Windows':
+        cmd = 'start'
+    elif userOS == 'Darwin':
+        cmd = 'open'
+    elif userOS == 'Linux':
+        cmd = 'xdg-open'
 
-	try:
-		os.system(cmd + " " + url)
-	except:
-		print(url)
+    try:
+        os.system(cmd + " " + url)
+    except:
+        print(url)
 
 if __name__ == '__main__':
-	jsonfile, html_page = init(sys.argv)
-	data = loadfile(jsonfile)
-	url = generateurl(html_page, data)
-	openurl(url)
+    jsonfile, html_page = init(sys.argv)
+    data = loadfile(jsonfile)
+    url = generateurl(html_page, data)
+    openurl(url)
