c0999b04d0edb00ab9267f06aafb78cf7ffc2e18
Mark Chen <9006264+Mark9804@users.noreply.github.com>
2021-12-26 22:46:16 +0900

    menu.js: 如果推荐了太多高热量食物，加一句多吃蔬菜

--- src/plugins/tools/menu.js
+++ src/plugins/tools/menu.js
@@ -8,8 +8,9 @@ function menu(msg) {
 早餐：${breakfast ? breakfast[getRandomInt(breakfast.length) - 1] : food}
 午餐：${lunch ? lunch[getRandomInt(lunch.length) - 1] : food}
 晚餐：${dinner ? dinner[getRandomInt(dinner.length) - 1] : food}`;
+  const eatMoreVegetable = message.match(/[煎炸炒烤油]/) ? "\n记得多吃蔬菜水果，不要上火哦。" : "";
 
-  msg.bot.say(msg.sid, message, msg.type, msg.uid, true);
+  msg.bot.say(msg.sid, `${message}${eatMoreVegetable}`, msg.type, msg.uid, true);
 }
 
 export { menu };
