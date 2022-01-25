e63ebc49b74c31e4bdfdc085d374b5da952564bc
Qin Fandong <shell_way@foxmail.com>
2022-01-25 12:12:27 +0800

    better html tag

--- src/views/component/common/README.txt
+++ src/views/component/common/README.txt
@@ -1,2 +1,5 @@
 ./param.js
   getParams               从 window.location.href 中获取参数
+
+./html.js
+  html                    为模板字符串打上 html 标签
--- src/views/component/common/html.js
+++ src/views/component/common/html.js
@@ -1,5 +1,12 @@
-function html(rawText) {
-  return rawText.raw.join("");
+function html(literals, ...placeholders) {
+  let context = "";
+
+  for (let i = 0; i < placeholders.length; ++i) {
+    context += literals[i];
+    context += placeholders[i].toString();
+  }
+
+  return context + literals[literals.length - 1];
 }
 
 export { html };
