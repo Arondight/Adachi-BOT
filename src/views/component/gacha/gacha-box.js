e60694b8cd1ab8f510ab8dc7d9caf7413f2d16ef
Mark Chen <9006264+Mark9804@users.noreply.github.com>
2022-01-27 22:34:39 +0900

    gacha: 更好的适配

--- src/views/component/gacha/gacha-box.js
+++ src/views/component/gacha/gacha-box.js
@@ -6,7 +6,7 @@ const template = html`<div
   class="gacha-box"
   :style="{ backgroundImage: 'url(' + item_props.item_rarity_background + ')' }"
 >
-  <img class="item-image" :src="item_props.image_url" />
+  <img class="item-image" :class="item_props.item_type" :class="item_props.item_rarity" :src="item_props.image_url" />
   <div class="container-item-props">
     <div v-if="item_props.item_label !== ''" class="item-label">{{ item_props.item_label }}</div>
     <img class="item-type-image" :src="item_props.item_type_image" />
@@ -35,14 +35,14 @@ export default defineComponent({
     };
 
     let item_props = {};
-    const imageType = typeMapping[props.data.item_type] || "角色";
+    const imageType = typeMapping[props.data.item_type] || "character";
     const imageName = props.data.item_name;
     const itemTypeImage = props.data.type;
     const itemRarity = rarityMapping[props.data.star] || "Four";
     const itemLabel =
       props.data.star === 5 ? "「" + props.data.times + "抽」" : props.isStat ? "「" + props.data.count + "次」" : "";
     const iconType = props.data.item_type === "角色" ? "element" : "type";
-
+    item_props.item_type = imageType;
     item_props.image_url = `http://localhost:9934/resources/Version2/wish/${imageType}/${imageName}.png`;
     item_props.item_rarity = itemRarity;
     item_props.item_type_image = `http://localhost:9934/resources/gacha/${iconType}/${itemTypeImage}.png`;
