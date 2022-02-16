2d978ce5d36765660c19cbb9cc50fd71b80da520
Mark Chen <9006264+Mark9804@users.noreply.github.com>
2022-02-16 19:12:51 +0900

    material: 武器加上武器名, 结构优化, Close #655

--- src/views/component/material/column.js
+++ src/views/component/material/column.js
@@ -1,11 +1,68 @@
 import { html } from "../common/html.js";
-import MaterialUnit from "./unit.js";
 
 // eslint-disable-next-line no-undef
-const { defineComponent, computed } = Vue;
+const { defineComponent } = Vue;
+
+const materialTemplate = html` <div class="unit">
+  <div class="top-box">
+    <div class="ascension-name">{{ ascensionName }}</div>
+    <div class="materials-list">
+      <img v-for="name in data.ascension" class="materials" :src="materialImage(name)" alt="ERROR" />
+    </div>
+  </div>
+  <div class="item-list">
+    <div class="container-item" v-for="item in params.list">
+      <img
+        class="item-image"
+        :src="itemImage(item.name, itemType)"
+        :class="rarityClass(item.rarity)"
+        :alt="item.name"
+      />
+      <div class="item-name-banner" v-if="itemType === 'weapon'">{{item.name}}</div>
+    </div>
+  </div>
+</div>`;
+
+const materialUnit = defineComponent({
+  name: "MaterialUnit",
+  template: materialTemplate,
+  props: {
+    data: Object,
+    type: String,
+  },
+  methods: {
+    materialImage: (name) => `http://localhost:9934/resources/Version2/info/image/${name}.png`,
+    itemImage: (name, itemType) => `http://localhost:9934/resources/Version2/thumb/${itemType}/${name}.png`,
+    rarityClass: (rarity) => {
+      const rarities = [undefined, "one-star", "two-star", "three-star", "four-star", "five-star"];
+      return rarities[rarity] || "four-star";
+    },
+  },
+  setup(props) {
+    const params = props.data;
+
+    let ascensionName = "";
+    if (props.type === "weapon") {
+      const de = params.ascension[0].split("的");
+      const zhi = params.ascension[0].split("之");
+      const arr = de.length === 1 ? zhi : de;
+      ascensionName = arr[0];
+    } else {
+      const regExp = new RegExp(/「(.*?)」/g);
+      ascensionName = regExp.exec(params.ascension[0])[0];
+    }
+
+    return {
+      params,
+      ascensionName,
+      itemType: props.type || "character",
+    };
+  },
+});
+
 const template = html`<div class="material-column">
   <div class="title">{{ title }}</div>
-  <MaterialUnit v-if="data.length !== 0" v-for="d in data" :data="d" :type="type" />
+  <materialUnit v-if="data.length !== 0" v-for="d in data" :data="d" :type="type" />
   <div v-else class="empty-box">今日没有可刷取材料的{{ type === "character" ? "角色" : "武器" }}</div>
 </div>`;
 
@@ -13,7 +70,7 @@ export default defineComponent({
   name: "MaterialColumn",
   template,
   components: {
-    MaterialUnit,
+    materialUnit,
   },
   props: {
     data: Array,
@@ -21,7 +78,7 @@ export default defineComponent({
     day: String,
   },
   setup(props) {
-    const title = computed(() => `${props.day}${props.type === "weapon" ? "武器" : "角色"}素材`);
+    const title = `${props.day}${props.type === "weapon" ? "武器" : "角色"}素材`;
     return { title };
   },
 });
