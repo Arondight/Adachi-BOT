afad6439f51bcdee14c61232c581e0ada633a751
Mark Chen <9006264+Mark9804@users.noreply.github.com>
2022-02-26 18:28:46 +0900

    material.js: 更新2.5周本

--- src/plugins/material/material.js
+++ src/plugins/material/material.js
@@ -5,7 +5,7 @@ import { render } from "#utils/render";
 import { getWordByRegex } from "#utils/tools";
 
 const getUrl = (p) => `https://upload-bbs.mihoyo.com/upload/${"/" === p[0] ? p.substring(1) : p}`;
-const urls = { weekly: getUrl("/2022/01/05/75833613/93626b52c4c06b537783346dfb6a7126_4957845255838694552.png") };
+const urls = { weekly: getUrl("/2022/02/18/75833613/15b472dcd67a67016ece772e8528faf0_2513941823560578118.png") };
 
 async function doMaterial(msg, url) {
   const cacheDir = path.resolve(global.rootdir, "data", "image", "material");
