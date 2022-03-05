4e8645af29f76e60ed3cad02e59158aaae5c05b9
Qin Fandong <shell_way@foxmail.com>
2022-03-05 10:30:55 +0800

    修复在特定情况下米游社推送有可能多次发送部分内容的问题，Fix #687

--- src/jobs/news.js
+++ src/jobs/news.js
@@ -5,8 +5,6 @@ import { checkAuth } from "#utils/auth";
 import { getCache } from "#utils/cache";
 import db from "#utils/database";
 
-const running = { mysNewsNotice: false };
-
 function initDB() {
   for (const t of ["announcement", "event", "information"]) {
     if (!db.includes("news", "timestamp", { type: t })) {
@@ -50,81 +48,71 @@ async function mysNewsNotice(withImg = true) {
     return;
   }
 
-  if (true === running.mysNewsNotice) {
-    return;
-  }
-
   initDB();
-
-  // XXX no return before set this false {
-  running.mysNewsNotice = true;
 
   const cacheDir = path.resolve(global.rootdir, "data", "image", "news");
   const data = db.get("news", "data");
+  const silent = {};
+  const news = [];
 
   for (const t of Object.keys(data)) {
     if (!lodash.hasIn(data[t], "data.list") || !Array.isArray(data[t].data.list)) {
       continue;
     }
 
-    const news = data[t].data.list;
+    silent[t] = 0 === db.get("news", "timestamp", { type: t }).time;
 
-    for (const n of lodash.sortBy(news, (c) => c.post.created_at).reverse()) {
+    for (const n of lodash.sortBy(data[t].data.list, (c) => c.post.created_at)) {
       if (!lodash.hasIn(n, "post")) {
         continue;
       }
 
-      const timestamp = db.get("news", "timestamp");
-      let lastTimeStamp = (timestamp.find((c) => t === c.type) || {}).time || 0;
-      const silent = 0 === lastTimeStamp;
+      const mkContent = (c) => ("。！？～~".split("").includes(c[c.length - 1]) ? c : `${c} ……`);
       const post = n.post || {};
       const { subject, content } = post;
-      let image;
-
-      if (true === withImg && "string" === typeof post.images[0]) {
-        try {
-          image = await getCache(post.images[0], cacheDir, "base64");
-        } catch (e) {
-          // do nothing
-        }
-      }
-
-      const imageCQ = undefined !== image ? `[CQ:image,type=image,file=base64://${image}]` : "";
+      const image = post.images[0];
       const url = "string" === typeof post.post_id ? `https://bbs.mihoyo.com/ys/article/${post.post_id}` : "";
       const items = [
         "string" === typeof subject ? subject : "",
-        imageCQ,
-        "string" === typeof content
-          ? "。！？～~".split("").includes(content[content.length - 1])
-            ? content
-            : `${content} ……`
-          : "",
+        "string" === typeof image ? image : "",
+        "string" === typeof content ? mkContent(content) : "",
         url,
       ];
       const stamp = post.created_at || 0;
+      const { time: timestamp } = db.get("news", "timestamp", { type: t });
 
-      // 立即写入，忽略所有的发送失败
-      db.update("news", "timestamp", { type: t }, { time: Math.max(stamp, lastTimeStamp) });
+      db.update("news", "timestamp", { type: t }, { time: Math.max(stamp, timestamp) });
 
-      if (false === silent && stamp > lastTimeStamp) {
-        if (lodash.some(items, (c) => "string" === typeof c && "" !== c)) {
-          const message = items.filter((c) => "string" === typeof c && "" !== c).join("\n");
-
-          for (const bot of global.bots) {
-            const ms = bot.boardcast(
-              message,
-              "group",
-              (c) => false !== checkAuth({ sid: c.group_id }, global.innerAuthName.mysNews, false)
-            );
-            await new Promise((resolve) => setTimeout(resolve, ms));
-          }
-        }
+      if (false === silent[t] && stamp > timestamp) {
+        news.push(items);
       }
     }
   }
 
-  running.mysNewsNotice = false;
-  // }
+  for (const n of lodash.sortBy(news, (n) => n.stamp)) {
+    let image64;
+
+    if (true === withImg && "string" === typeof n[1]) {
+      try {
+        image64 = await getCache(n[1], cacheDir, "base64");
+      } catch (e) {
+        // do nothing
+      }
+    }
+
+    n[1] = undefined === image64 ? "" : `[CQ:image,type=image,file=base64://${image64}]`;
+
+    const message = n.filter((c) => "string" === typeof c && "" !== c).join("\n");
+
+    for (const bot of global.bots) {
+      const ms = bot.boardcast(
+        message,
+        "group",
+        (c) => false !== checkAuth({ sid: c.group_id }, global.innerAuthName.mysNews, false)
+      );
+      await new Promise((resolve) => setTimeout(resolve, ms));
+    }
+  }
 }
 
 export { mysNewsNotice, mysNewsTryToResetDB, mysNewsUpdate };
