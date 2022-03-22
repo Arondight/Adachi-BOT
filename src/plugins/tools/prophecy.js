60b24bc3741136fee3fda00d2ed21055694a830d
BTMUli <BT-Muli@outlook.com>
2022-03-22 14:21:08 +0800

    参考占卜抄了个御神签的插件
    这个插件的数据源用的是鸣神大社的御神签数据，逻辑跟占卜的是一样的，数据以后再补充

--- src/plugins/tools/prophecy.js
+++ src/plugins/tools/prophecy.js
@@ -2,7 +2,7 @@ import { getRandomInt } from "#utils/tools";
 
 async function prophecy(msg) {
   const data = global.prophecy.data[getRandomInt(global.prophecy.data.length)];
-  const message = `${data.lucky}\n${data.summary}：${data.text}。\n${data.annotation}`;
+  const message = `${data.summary}\n${data.lucky}\n${data.text[getRandomInt(data.text.length)]}`;
   msg.bot.say(msg.sid, message, msg.type, msg.uid, true, "\n");
 }
 
