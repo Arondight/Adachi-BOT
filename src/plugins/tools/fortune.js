3d3256bdf9af315e823d476f1552e95c7be8b685
BTMUli <BT-Muli@outlook.com>
2022-03-22 16:41:19 +0800

    改回数据
    把占卜的逻辑改回去了，以后就只用御神签的command好了

--- src/plugins/tools/fortune.js
+++ src/plugins/tools/fortune.js
@@ -3,9 +3,9 @@ import { getRandomInt } from "#utils/tools";
 async function fortune(msg) {
   const luck = global.fortune.luck[getRandomInt(global.fortune.luck.length)];
   const thing = global.fortune.things[getRandomInt(global.fortune.things.length)];
-  const pre = "今天的幸运物是：" + `${thing.item}`;
+  const pre = "==========\n今天的幸运物是：" + `${thing.item}\n`;
   const message = `${luck.summary}\n${luck.lucky}\n${luck.text[getRandomInt(luck.text.length)]}
-                    \n${pre}\n${thing.description}\n`;
+                    \n${pre}\n${thing.description}`;
   msg.bot.say(msg.sid, message, msg.type, msg.uid, true, "\n");
 }
 
