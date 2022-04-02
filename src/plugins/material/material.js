3dd97191a524dfbb51df47c88d1e822d7fc161d4
Xm798 <git@xm.mk>
2022-04-02 11:15:38 +0800

    material.js: 更新周本材料图

--- src/plugins/material/material.js
+++ src/plugins/material/material.js
@@ -4,7 +4,7 @@ import { getCache } from "#utils/cache";
 import { render } from "#utils/render";
 import { getWordByRegex } from "#utils/tools";
 
-const mUrls = { weekly: getUrl("/2022/02/18/75833613/15b472dcd67a67016ece772e8528faf0_2513941823560578118.png") };
+const mUrls = { weekly: getUrl("/2022/03/30/75379475/f13d80f2e7f83b667b7f3ffcc36647d2_4239005231258368385.png") };
 
 function getUrl(p) {
   return `https://upload-bbs.mihoyo.com/upload/${"/" === p[0] ? p.substring(1) : p}`;
