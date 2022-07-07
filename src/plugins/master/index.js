7a7f4d5f8e16e80dcba857eab69102eefa166d6a
Qin Fandong <shell_way@foxmail.com>
2022-07-07 11:28:13 +0800

    开发文档中添加权限相关说明

--- src/plugins/master/index.js
+++ src/plugins/master/index.js
@@ -10,25 +10,12 @@ async function Plugin(msg) {
     return [id, isOn];
   }
 
-  const all = [
-    "artifact_auth",
-    "character_overview_auth",
-    "feedback_auth",
-    "fun_auth",
-    "gacha_auth",
-    "music_auth",
-    "mys_news_auth",
-    "query_gameinfo_auth",
-    "rating_auth",
-    "reply_auth",
-  ];
-
   if (hasEntrance(msg.text, "master", "refresh_wish_detail")) {
     msg.bot.say(msg.sid, `卡池内容${gachaUpdate() ? "已刷新" : "刷新失败"}。`, msg.type, msg.uid, true);
     return;
   }
 
-  for (const auth of all) {
+  for (const auth of Object.keys(global.authority.default || [])) {
     if (hasEntrance(msg.text, "master", auth)) {
       setAuth(msg, global.authority.setting[auth] || [], ...parse(msg.text, auth));
       break;
