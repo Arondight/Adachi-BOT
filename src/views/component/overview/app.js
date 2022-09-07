4bf9c714e30732a7761210cabbaf982c2484605d
Mark Chen <9006264+Mark9804@users.noreply.github.com>
2022-09-07 12:02:56 +0900

    小虫子露出黑脚了 #885

--- src/views/component/overview/app.js
+++ src/views/component/overview/app.js
@@ -3,10 +3,10 @@ import { getParams, html } from "../common/utils.js";
 const { defineComponent, defineAsyncComponent } = window.Vue;
 
 const template = html`<div class="background" :class="backgroundStyle">
-  <img class="svg" :src="elementSvgSrc" v-if="itemType === 'character'" />
-  <characterInfoBox v-if="itemType === 'character'" :data="params" />
-  <weaponInfoBox v-if="itemType === 'weapon'" :data="params" />
-  <div class="credit">Created by Adachi-BOT</div>
+    <img class="svg" :src="elementSvgSrc" v-if="'角色' === itemType" />
+    <characterInfoBox v-if="'角色' === itemType" :data="params" />
+    <weaponInfoBox v-if="'武器' === itemType" :data="params" />
+    <div class="credit">Created by Adachi-BOT</div>
 </div>`;
 const characterInfoBox = defineAsyncComponent(() => import("./characterInfoBox.js"));
 const weaponInfoBox = defineAsyncComponent(() => import("./weaponInfoBox.js"));
@@ -20,11 +20,6 @@ export default defineComponent({
   },
   setup: function () {
     const params = getParams(window.location.href);
-    // noinspection NonAsciiCharacters
-    const typeMap = {
-      角色: "character",
-      武器: "weapon",
-    };
     const rarityMap = {
       5: "five",
       4: "four",
@@ -32,23 +27,17 @@ export default defineComponent({
       2: "two",
       1: "one",
     };
-    // noinspection NonAsciiCharacters
-    const elementMap = {
-      风: "anemo",
-      冰: "cryo",
-      草: "dendro",
-      雷: "electro",
-      岩: "geo",
-      水: "hydro",
-      火: "pyro",
-    };
-    const itemType = typeMap[params.type] || "weapon";
-    const charElementType = itemType === "character" ? elementMap[params.element.slice()[0]] || "anemo" : "";
+    const itemType = params.type;
     const itemRarity = rarityMap[params.rarity] || "4";
-    const backgroundStyle =
-      itemType === "character" ? "character-" + charElementType : "weapon-" + itemRarity + "-stars";
-    const elementSvgSrc =
-      itemType === "character" ? `http://localhost:9934/resources/Version2/elements/${charElementType}.svg` : "";
+    let backgroundStyle = `weapon-${itemRarity}-stars`;
+    let elementSvgSrc = "";
+
+    if ("角色" === itemType) {
+      const charElementType = params.element || "风元素";
+
+      backgroundStyle = `character-${charElementType}`;
+      elementSvgSrc = `http://localhost:9934/resources/elements/picture/${charElementType}.svg`;
+    }
 
     return {
       params,
