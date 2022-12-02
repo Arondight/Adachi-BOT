c8a420ce3ca8d4be8184bebd94b832f61b978c92
Qin Fandong <shell_way@foxmail.com>
2022-12-02 16:20:44 +0800

    fix JS module CI, fix #967

--- data/js/update.sh
+++ data/js/update.sh
@@ -4,9 +4,9 @@ RDIR=$(dirname $(readlink -f "$0"))
 CURL=('curl' '-sL')
 
 typeset -A LOCPATH=(
-  ['https://cn.vuejs.org/js/vue.js']=\
+  ['https://unpkg.com/vue@2/dist/vue.js']=\
 "${RDIR}/vue2.js"
-  ['https://cn.vuejs.org/js/vue.min.js']=\
+  ['https://unpkg.com/vue@2/dist/vue.min.js']=\
 "${RDIR}/vue2.min.js"
   ['https://unpkg.com/vue@next/dist/vue.global.js']=\
 "${RDIR}/vue3.global.js"
